2b9164d031b98d6abc4156722c33ae4aa481b0a0
arunc700 <48315448+arunc700@users.noreply.github.com>
2019-09-03 18:36:26 +0530

    Upload file

---
+++ FactoryMethodPattern/Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.FactoryMethodPattern
+{
+    /// <summary>
+    /// Products 
+    /// </summary>
+    public abstract class Member
+    {
+        public abstract void Register();
+        public abstract void Notify();
+    }
+}
---
+++ FactoryMethodPattern/MemberManagement.cs
@@ -0,0 +1,31 @@
+﻿using DesignPattern.FactoryMethodPattern.Branches.India;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.FactoryMethodPattern
+{
+    public static class MemberManagement
+    {
+        public static Member CreateMember(string type)
+        {
+            Member member;
+            if (type == "A")
+            {
+                member = new AnualMembership();
+            }
+            else if (type == "L")
+            {
+                member = new LifeTimeMembership();
+            }
+            else
+            {
+                member = new TempMembership();
+            }
+
+            return member;
+        }
+
+
+    }
+}
